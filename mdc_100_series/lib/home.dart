87ecb56ac4d6f732839dc3935264348eb2ad2348
Tatsuya Inokuchi <ino1221@gmail.com>
2022-01-31 10:48:56 +0900

    feat: home.dart app_bar

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -23,10 +23,42 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return const Scaffold(
+    return Scaffold(
       // TODO: Add app bar (102)
       // TODO: Add a grid view (102)
-      body: Center(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
+        title: const Text('SHRINE'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: () {
+              print('Search button');
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: () {
+              print('Filter button');
+            },
+          ),
+        ],
+      ),
+      body: const Center(
         child: Text('You did it!'),
       ),
       // TODO: Set resizeToAvoidBottomInset (101)
