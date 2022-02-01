9a1a346b1576defabb1cef67f4457daa04ee009b
Tatsuya Inokuchi <ino1221@gmail.com>
2022-02-01 14:07:42 +0900

    feat: Add a card in a grid

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -58,8 +58,34 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: Text('You did it!'),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        children: <Widget>[
+          Card(
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 18.0 / 11.0,
+                    child: Image.asset('assets/diamond.png'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const <Widget>[
+                        Text('Title'),
+                        SizedBox(height: 8.0),
+                        Text('Secondary Text'),
+                      ],
+                    ),
+                  ),
+                ],
+              ))
+        ],
       ),
       // TODO: Set resizeToAvoidBottomInset (101)
       resizeToAvoidBottomInset: false,
